6c10899b1402c4bebfd14b3949111f85c1e62e42
nicky <362330721@qq.com>
2018-04-18 16:28:28 +0800

    提交全局搜索引擎工程，采用Apache Lucene搭建

---
+++ src/taoshop-common/src/test/java/com/mq/test/Consumer.java
@@ -0,0 +1,34 @@
+package com.mq.test;
+
+import com.alibaba.rocketmq.client.consumer.DefaultMQPushConsumer;
+import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import com.alibaba.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import com.alibaba.rocketmq.client.exception.MQClientException;
+import com.alibaba.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+public class Consumer {
+    public static void main(String[] args) throws MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(
+                "rmq-group");
+
+        consumer.setNamesrvAddr("123.207.63.192:9876");
+        consumer.setInstanceName("consumer");
+        consumer.subscribe("TopicA-test", "TagA");
+
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(
+                    List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                for (MessageExt msg : msgs) {
+                    System.out.println(new String(msg.getBody()));
+                }
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        consumer.start();
+        System.out.println("Consumer Started.");
+    }
+}
