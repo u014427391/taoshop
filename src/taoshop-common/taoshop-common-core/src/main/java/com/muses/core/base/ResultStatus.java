340d8d73560b1a48f732e4a415fabc2c2ba03d1a
nicky <362330721@qq.com>
2018-05-20 11:40:35 +0800

    core工程提交代码

---
+++ src/taoshop-common/taoshop-common-core/src/main/java/com/muses/core/base/ResultStatus.java
@@ -0,0 +1,33 @@
+package com.muses.core.base;
+
+/**
+ * <pre>
+ *  TODO 类说明
+ * </pre>
+ *
+ * @author nicky.ma
+ * @version 1.00.00
+ * <pre>
+ * 修改记录
+ *    修改后版本:     修改人：  修改日期:     修改内容:
+ * </pre>
+ */
+public enum ResultStatus {
+
+    //TODO add result status
+    OK("200","OK","成功"),
+    VALIDATION_ERROR("400","Validation Error","参数校验异常"),
+    AUTHENTICATION_ERROR("300","Authentication Error","身份认证异常");
+
+    private final String statusCode;
+
+    private final String resonPhrase;
+
+    private final String message;
+
+    private ResultStatus(String statusCode, String resonPhrase, String message) {
+        this.statusCode = statusCode;
+        this.resonPhrase = resonPhrase;
+        this.message = message;
+    }
+}
