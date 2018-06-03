08bdf232eb3a991fc46950708df61dc6948d88ee
nicky <362330721@qq.com>
2018-06-03 18:13:42 +0800

    修改门户网站代码

--- src/taoshop-portal/src/main/java/com/muses/taoshop/portal/Application.java
+++ src/taoshop-portal/src/main/java/com/muses/taoshop/portal/Application.java
@@ -1,15 +1,37 @@
 package com.muses.taoshop.portal;
 
 
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.*;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.*;
-
+/**
+ *
+ * <pre>
+ *  SpringBoot启动配置类
+ * </pre>
+ * @author nicky
+ * @version 1.00.00
+ * <pre>
+ * 修改记录
+ *    修改后版本:     修改人：  修改日期:     修改内容:
+ * </pre>
+ */
 @Controller
+@EnableScheduling
+@EnableTransactionManagement
+//@EnableCaching
+@EnableAsync
 @EnableAutoConfiguration
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,})
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class, MybatisAutoConfiguration.class})
 public class Application {
 
     @RequestMapping("/")
