d9c123798234c54f9f094193c998b8971bc48a0c
Nicky <362330721@qq.com>
2018-06-03 17:11:23 +0800

    添加WebConfig配置类

---
+++ src/taoshop-portal/src/main/java/com/muses/taoshop/portal/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.muses.taoshop.portal.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+  
+    @Override  
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+  
+        registry.addResourceHandler("/templates/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX+"/templates/");
+        registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX+"/static/");  
+        super.addResourceHandlers(registry);  
+    }  
+  
+}  
