3393bfce5bff0376067cd79ebfbf920b56623c31
u014427391 <3623307211@qq.com>
2018-06-10 00:45:07 +0800

    添加VO类

--- src/taoshop-provider-api/taoshop-provider-api-item/src/main/java/com/muses/taoshop/vo/ItemCategory.java
+++ src/taoshop-provider-api/taoshop-provider-api-item/src/main/java/com/muses/taoshop/vo/ItemCategory.java
@@ -2,15 +2,41 @@ package com.muses.taoshop.vo;
 
 import java.util.Date;
 
+/**
+ * <pre>
+ *  商品品类
+ * </pre>
+ * @author nicky
+ * @version 1.00.00
+ * <pre>
+ * 修改记录
+ *    修改后版本:     修改人：  修改日期: 2018.06.09 21:49    修改内容:
+ * </pre>
+ */
 public class ItemCategory {
+    /**
+     * 商品品类id
+     */
     private Long id;
 
+    /**
+     * 商品品类名称
+     */
     private String categoryName;
 
+    /**
+     * 上级id
+     */
     private Long sjid;
 
+    /**
+     * 上次修改时间
+     */
     private Date lastModifyTime;
 
+    /**
+     * 创建时间
+     */
     private Date createTime;
 
     public Long getId() {
@@ -24,6 +50,7 @@ public class ItemCategory {
     public String getCategoryName() {
         return categoryName;
     }
+
 
     public void setCategoryName(String categoryName) {
         this.categoryName = categoryName == null ? null : categoryName.trim();
