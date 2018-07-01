06147630cdfa559c0ea8e3f5568fea34e40ec340
Nicky <362330721@qq.com>
2018-07-01 19:31:39 +0800

    后台管理系统工程改造

---
+++ src/taoshop-manager/taoshop-manager-api/src/main/java/com/muses/taoshop/sys/service/ISysUserService.java
@@ -0,0 +1,17 @@
+package com.muses.taoshop.sys.service;
+
+/**
+ * <pre>
+ *  TODO 类说明
+ * </pre>
+ *
+ * @author nicky
+ * @version 1.00.00
+ * <pre>
+ * 修改记录
+ *    修改后版本:     修改人：  修改日期: 2018.07.01 19:09    修改内容:
+ * </pre>
+ */
+public interface ISysUserService {
+
+}
