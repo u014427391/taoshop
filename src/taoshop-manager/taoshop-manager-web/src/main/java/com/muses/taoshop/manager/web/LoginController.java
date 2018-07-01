de54f2a07237f5379a4a683feaf0f79e1dcf91fa
u014427391 <3623307211@qq.com>
2018-07-01 22:16:10 +0800

    电商运营平台改造

--- src/taoshop-manager/taoshop-manager-web/src/main/java/com/muses/taoshop/manager/web/LoginController.java
+++ src/taoshop-manager/taoshop-manager-web/src/main/java/com/muses/taoshop/manager/web/LoginController.java
@@ -1,5 +1,10 @@
 package com.muses.taoshop.manager.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * <pre>
  *  TODO 类说明
@@ -12,5 +17,17 @@ package com.muses.taoshop.manager.web;
  *    修改后版本:     修改人：  修改日期: 2018.07.01 21:01    修改内容:
  * </pre>
  */
-public class LoginController {
+@Controller
+@RequestMapping("/admin")
+public class LoginController extends BaseController{
+
+    @RequestMapping(value = "/toLogin")
+    @GetMapping
+    public ModelAndView toLogin(){
+        ModelAndView mv = this.getModelAndView();
+        mv.setViewName("login");
+        return mv;
+    }
+
+
 }
