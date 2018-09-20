616af6301d9bac4bb46a9721dc341eb62b0154f7
Nicky <362330721@qq.com>
2018-09-20 23:58:05 +0800

    用户中心页面

--- src/taoshop-manager/taoshop-manager-web/src/main/java/com/muses/taoshop/manager/web/controller/UserController.java
+++ src/taoshop-manager/taoshop-manager-web/src/main/java/com/muses/taoshop/manager/web/controller/UserController.java
@@ -1,5 +1,10 @@
 package com.muses.taoshop.manager.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * <pre>
  *  用户中心控制类
@@ -12,7 +17,18 @@ package com.muses.taoshop.manager.web.controller;
  *    修改后版本:     修改人：  修改日期: 2018.08.11 11:06    修改内容:
  * </pre>
  */
+@Controller
+@RequestMapping("/user")
 public class UserController extends BaseController{
 
-
+    /**
+     * 跳转到用户中心
+     * @return
+     */
+    @GetMapping(value = "/toUserCenter")
+    public ModelAndView toUserCenter() {
+        ModelAndView mv = this.getModelAndView();
+        mv.setViewName("admin/user/user_center");
+        return mv;
+    }
 }
