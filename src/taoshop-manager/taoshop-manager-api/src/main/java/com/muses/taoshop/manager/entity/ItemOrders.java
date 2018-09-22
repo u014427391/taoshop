d8be064adbc0a33f9501c6bc9cbc7c263452d847
u014427391 <3623307211@qq.com>
2018-09-22 12:36:46 +0800

    初步提交一下菜单管理&订单管理的接口类

---
+++ src/taoshop-manager/taoshop-manager-api/src/main/java/com/muses/taoshop/manager/entity/ItemOrders.java
@@ -0,0 +1,20 @@
+package com.muses.taoshop.manager.entity;
+
+import lombok.Data;
+
+/**
+ * <pre>
+ *  订单实体类
+ * </pre>
+ *
+ * @author nicky
+ * @version 1.00.00
+ * <pre>
+ * 修改记录
+ *    修改后版本:     修改人：  修改日期: 2018.09.22 11:09    修改内容:
+ * </pre>
+ */
+@Data
+public class ItemOrders {
+
+}
---
+++ src/taoshop-manager/taoshop-manager-api/src/main/java/com/muses/taoshop/manager/entity/Menu.java
@@ -0,0 +1,16 @@
+package com.muses.taoshop.manager.entity;
+
+/**
+ * <pre>
+ *  菜单信息实体类
+ * </pre>
+ *
+ * @author nicky
+ * @version 1.00.00
+ * <pre>
+ * 修改记录
+ *    修改后版本:     修改人：  修改日期: 2018.09.22 12:32    修改内容:
+ * </pre>
+ */
+public class Menu {
+}
