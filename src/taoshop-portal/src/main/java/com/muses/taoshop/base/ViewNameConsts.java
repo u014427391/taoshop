322aeb124448c7ec583d8eecc445722de4507670
Nicky <3623307211@qq.com>
2018-09-24 10:02:32 +0800

    修缮一下门户网站功能

---
+++ src/taoshop-portal/src/main/java/com/muses/taoshop/base/ViewNameConsts.java
@@ -0,0 +1,17 @@
+package com.muses.taoshop.base;
+
+/**
+ * <pre>
+ *  ModeAndView的viewName配置
+ * </pre>
+ *
+ * @author nicky
+ * @version 1.00.00
+ * <pre>
+ * 修改记录
+ *    修改后版本:     修改人：  修改日期: 2018.09.23 22:47    修改内容:
+ * </pre>
+ */
+public class ViewNameConsts {
+
+}
