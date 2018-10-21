98533a7e1921f51d42ee62c3e074eeba62966ba6
nicky <3623307211@qq.com>
2018-10-22 00:04:00 +0800

    权限菜单初步实现

---
+++ src/taoshop-manager/taoshop-manager-service/src/main/java/com/muses/taoshop/manager/mapper/SysPermissionMapper.java
@@ -0,0 +1,16 @@
+package com.muses.taoshop.manager.mapper;
+
+/**
+ * <pre>
+ *  TODO 类说明
+ * </pre>
+ *
+ * @author nicky
+ * @version 1.00.00
+ * <pre>
+ * 修改记录
+ *    修改后版本:     修改人：  修改日期: 2018.10.21 23:30    修改内容:
+ * </pre>
+ */
+public interface SysPermissionMapper {
+}
