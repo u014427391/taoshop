ec6fbc94c1f719099fa15ae044d194ab3cf3ffd5
nicky <3623307211@qq.com>
2018-10-22 22:54:45 +0800

    权限菜单获取方法二

--- src/taoshop-manager/taoshop-manager-api/src/main/java/com/muses/taoshop/manager/service/IMenuService.java
+++ src/taoshop-manager/taoshop-manager-api/src/main/java/com/muses/taoshop/manager/service/IMenuService.java
@@ -6,5 +6,16 @@ import com.muses.taoshop.manager.entity.Menu;
 import java.util.List;
 
 public interface IMenuService {
+    /**
+     * 根据权限id获取菜单
+     * @param permissionId
+     * @return
+     */
     Menu listMenu(int permissionId);
+
+    /**
+     * 权限菜单获取，即根据用户角色获取用户可以查看的菜单
+     * @return
+     */
+    List<Menu> listPermissionMenu(int userId);
 }
