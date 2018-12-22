7d2d49d32898cc25f4c00e873245fb1116c68276
nicky <3623307211@qq.com>
2018-12-22 23:41:03 +0800

    ThreadLocal test

---
+++ src/taoshop-portal/src/test/java/com/muses/taoshop/ThreadLocalTest.java
@@ -0,0 +1,16 @@
+package com.muses.taoshop;
+
+/**
+ * <pre>
+ *  TODO 类说明
+ * </pre>
+ *
+ * @author nicky
+ * @version 1.00.00
+ * <pre>
+ * 修改记录
+ *    修改后版本:     修改人：  修改日期: 2018.12.22 22:51    修改内容:
+ * </pre>
+ */
+public class ThreadLocalTest {
+}
